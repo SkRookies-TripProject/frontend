205564ac86e1cd70734853cff38210193429667d
Hyein <hi020514@yourdomain.com>
2026-04-02 15:04:21 +0900

    api 파일 추가

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://25.2.109.64:8080",
+  baseURL: "http://25.2.109.64:8080/api",
 });
+
+//  요청마다 토큰 자동 추가
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default instance;
